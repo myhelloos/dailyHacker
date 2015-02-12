6e4ee2b4f4404d1ea50ce6ea46f463274d328b8e
Alfred Yuan <yuanxun.cn@gmail.com>
2015-02-12 18:29:33 +0800

    12/02的 learn python the hard way

---
+++ python/learnPython/020FunctionsAndFiles.py
@@ -0,0 +1,35 @@
+from sys import argv
+
+script, inputFile = argv
+
+def printAll(f):
+    print f.read()
+
+def rewind(f):
+    f.seek(0)
+
+def printALine(lineCount, f):
+    print lineCount, f.readline();
+
+currentFile = open(inputFile)
+
+print "First let's print the whole file:\n"
+
+printAll(currentFile)
+
+print "Now let's rewind, kind of like a tape."
+
+rewind(currentFile)
+
+print "Let's print these lines:"
+
+currentLine = 1
+printALine(currentLine, currentFile)
+
+currentLine += 1
+printALine(currentLine, currentFile)
+
+currentLine +=  1
+printALine(currentLine, currentFile)
+
+currentFile.close()
