6c16c629e7062068bed3c606ca409e2762ce2c8c
Alfred Yuan <yuanxun.cn@gmail.com>
2015-02-12 15:17:27 +0800

    12/02的第三次dataStructure in js

--- js/hashing/HashTable.js
+++ js/hashing/HashTable.js
@@ -26,12 +26,12 @@ function simpleHash(data) {
     for (var i = 0; i < data.length; i++) {
         total += data.charCodeAt(i);
     }
-    print("Hash Value: " + data + " -> " + total);
+    // print("Hash Value: " + data + " -> " + total);
     return total % this.table.length;
 }
 
 function betterHash(data) {
-    const H = 31;
+    const H = 39;
     var total = 0;
     for (var i = 0; i < data.length; i++) {
         total += H * total + data.charCodeAt(i);
@@ -40,6 +40,6 @@ function betterHash(data) {
     if (total < 0) {
         total += this.table.length - 1;
     }
-    print("Hash Value: " + data + " -> " + parseInt(total));
+    // print("Hash Value: " + data + " -> " + parseInt(total));
     return parseInt(total);
 }
