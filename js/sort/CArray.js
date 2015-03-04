9e1e77269b762d4c6e9a55cb5f6b543252d9073d
Alfred Yuan <yuanxun.cn@gmail.com>
2015-03-04 23:09:13 +0800

    Data Structures and Algorithms with js: Sort, insertion sort

--- js/sort/CArray.js
+++ js/sort/CArray.js
@@ -1,82 +1,95 @@
 function CArray(numElements) {
-    this.dataStore = [];
-    this.pos = 0;
-    this.numElements = numElements;
-    this.insert = insert;
-    this.toString = toString;
-    this.clear = clear;
-    this.setData = setData;
-    this.swap = swap;
+		this.dataStore = [];
+		this.pos = 0;
+		this.numElements = numElements;
+		this.insert = insert;
+		this.toString = toString;
+		this.clear = clear;
+		this.setData = setData;
+		this.swap = swap;
 
-    this.bubbleSort = bubbleSort;
-    this.selectionSort = selectionSort;
+		this.bubbleSort = bubbleSort;
+		this.selectionSort = selectionSort;
+		this.insertionSort = insertionSort;
 
-    for (var i = 0; i < numElements; i++) {
-        this.dataStore[i] = i;
-    }
-}
-/*******************************************************************************
-                                    Basic Sort Algorithm
-*******************************************************************************/
+		for (var i = 0; i < numElements; i++) {
+			this.dataStore[i] = i;
+		}
+	}
+	/*******************************************************************************
+	                                    Basic Sort Algorithm
+	*******************************************************************************/
 function bubbleSort() {
-    var numElements = this.dataStore.length;
-    var temp;
-    for (var outer = numElements; outer >= 2; outer--) {
-        for (var inner = 0; inner <= outer-1; inner++) {
-            if (this.dataStore[inner] > this.dataStore[inner+1]) {
-                swap(this.dataStore, inner, inner+1);
-            }
-        }
-    }
+	var numElements = this.dataStore.length;
+	var temp;
+	for (var outer = numElements; outer >= 2; outer--) {
+		for (var inner = 0; inner <= outer - 1; inner++) {
+			if (this.dataStore[inner] > this.dataStore[inner + 1]) {
+				swap(this.dataStore, inner, inner + 1);
+			}
+		}
+	}
 }
 
 function selectionSort() {
-    var min, temp;
-    for (var outer = 0; outer < this.dataStore.length - 1; outer++) {
-        min = outer;
-        for (var inner = outer + 1; inner < this.dataStore.length; inner++) {
-            if (this.dataStore[inner] < this.dataStore[min]) {
-                min = inner;
-            }
-        }
-        swap(this.dataStore, outer, min);
-    }
+	var min, temp;
+	for (var outer = 0; outer < this.dataStore.length - 1; outer++) {
+		min = outer;
+		for (var inner = outer + 1; inner < this.dataStore.length; inner++) {
+			if (this.dataStore[inner] < this.dataStore[min]) {
+				min = inner;
+			}
+		}
+		swap(this.dataStore, outer, min);
+	}
 }
 
+function insertionSort() {
+	var temp, inner;
+	for (var outer = 1; outer < this.dataStore.length; outer++) {
+		temp = this.dataStore[outer];
+		inner = outer;
+		while (inner > 0 && (this.dataStore[inner - 1] >= temp)) {
+			this.dataStore[inner] = this.dataStore[inner - 1];
+			inner--;
+		}
+		this.dataStore[inner] = temp;
+	}
+}
 
 /*******************************************************************************
-                                    Array Test Bed
-*******************************************************************************/
+	                                    Array Test Bed
+	*******************************************************************************/
 function setData() {
-    for (var i = 0; i < this.numElements; i++) {
-        this.dataStore[i] = Math.floor(Math.random() *
-                (this.numElements+1))
-    }
+	for (var i = 0; i < this.numElements; i++) {
+		this.dataStore[i] = Math.floor(Math.random() *
+			(this.numElements + 1))
+	}
 }
 
 function clear() {
-    for (var i = 0; i < this.dataStore.length; i++) {
-        this.dataStore[i] = 0;
-    }
+	for (var i = 0; i < this.dataStore.length; i++) {
+		this.dataStore[i] = 0;
+	}
 }
 
 function insert(element) {
-    this.dataStore[this.pos++] = element;
+	this.dataStore[this.pos++] = element;
 }
 
 function toString() {
-    var retstr = "";
-    for (var i = 0; i < this.dataStore.length; i++) {
-        retstr += this.dataStore[i] + " ";
-        if (i > 0 && i % 10 == 0) {
-            retstr += "\n";
-        }
-    }
-    return retstr;
+	var retstr = "";
+	for (var i = 0; i < this.dataStore.length; i++) {
+		retstr += this.dataStore[i] + " ";
+		if (i > 0 && i % 10 == 0) {
+			retstr += "\n";
+		}
+	}
+	return retstr;
 }
 
 function swap(arr, index1, index2) {
-    var temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
+	var temp = arr[index1];
+	arr[index1] = arr[index2];
+	arr[index2] = temp;
 }
