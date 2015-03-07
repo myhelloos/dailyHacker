38168e7570f466269d0c25ec79f84b9ab89a37bf
Alfred Yuan <yuanxun.cn@gmail.com>
2015-03-07 23:41:59 +0800

    lear python the hard way: ex34: Accessing Elements of Lists

---
+++ python/learnPython/ex34.py
@@ -0,0 +1,33 @@
+animals = ['bear', 'python', 'peackck', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1.
+# the animal at 1 is "python"
+print animals[1], "(python?)"
+
+# The third(3rd) animal.
+# the third(3rd) animal is at 2 and it's "peackck"
+print animals[2], "(peackck?)"
+
+# The first(1st) animal.
+# The first(1st) animal is at 0 and it's "bear"
+print animals[0], "(bear?)"
+
+# The animal at 3.
+# The animal at 3 is "kangaroo"
+print animals[3], "(kangaroo?)"
+
+# The fifth(5th) animal.
+# the fifth(5th) animal is at 4 and it's "whale"
+print animals[4], "(whale?)"
+
+# The animal at 2.
+# the animal at 2 is "peackck"
+print animals[2], "(peackck?)"
+
+# The sixth(6th) animal.
+# the sixth(6th) animal is at 5 and it's "platyus"
+print animals[5], "(platyus?)"
+
+# The animal at 4.
+# the animal at 4 is "whale"
+print animals[4], "(whale?)"
