3102012da1892cab12a6d88b15d86e3b1cc38435
Alfred Yuan <yuanxun.cn@gmail.com>
2015-03-16 13:27:18 +0800

    cracking the coding interview: ex01 updated with BitMap

--- java/EX01_1.java
+++ java/EX01_1.java
@@ -1,14 +1,12 @@
 /**
 * 实现一个算法，确定一个字符串的所有字符是否全都不同。假如不允许使用额外的数据结构，又该如何处理
 */
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.BitSet;
 
 /**
 *   假定字符集为ASCII
 */
-public class EX01 {
+public class EX01_1 {
     /**
     * 使用java.util.BitSet
     */
@@ -61,7 +59,7 @@ public class EX01 {
     }
 
     public static void main(String[] args) {
-        EX01 test_base = new EX01();
+        EX01_1 test_base = new EX01_1();
 
         if (args.length > 0) {
             String test_string = args[0];
@@ -70,27 +68,6 @@ public class EX01 {
             System.out.println("result of isUniqueChars3: " + test_base.isUniqueChars3(test_string));
             return;
         }
-        System.out.println("Testing Begins:");
-        String[] test_string = new String[10];
-        Random rand = new Random();
-        for (int i = 0; i < test_string.length; i++) {
-            int random_len = rand.nextInt(256);
-            test_string[i] = random(random_len);
-        }
-        for (int i = 0; i < test_string.length; i++) {
-            System.out.println(test_string[i]);
-            System.out.println("result of isUniqueChars: " + test_base.isUniqueChars(test_string[i]));
-            System.out.println("result of isUniqueChars2: " + test_base.isUniqueChars2(test_string[i]));
-            System.out.println("result of isUniqueChars3: " + test_base.isUniqueChars3(test_string[i]));
-        }
-    }
-
-    /** 产生一个随机的字符串，适用于JDK 1.7 */
-    public static String random(int length) {
-        StringBuilder builder = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            builder.append((char) (ThreadLocalRandom.current().nextInt(33, 128)));
-        }
-        return builder.toString();
+        System.err.printf("Usage: java %s String\n", test_base.getClass().getSimpleName());
     }
 }
