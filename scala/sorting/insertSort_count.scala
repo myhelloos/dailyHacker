7d35840598543e6a2e3299b3ef6e3d13a4e3cd9a
Alfred Yuan <yuanxun.cn@gmail.com>
2015-04-06 19:01:23 +0800

    daily hacker, insertion sort improvement and count the shift

--- scala/sorting/insertSort_count.scala
+++ scala/sorting/insertSort_count.scala
@@ -5,11 +5,11 @@ object Solution {
         val arr = readLine.split(" ").map(_.toInt)
 
         insertSort(arr)
-        printArray(arr)
     }
 
 
     def insertSort(arr: Array[Int]): Unit = {
+        var result = 0
         for (index <- 1 until arr.length) {
             val item = arr(index)
             var hole = index;
@@ -17,8 +17,10 @@ object Solution {
                 arr(hole) = arr(hole - 1)
                 hole -= 1
             }
+            result += (index - hole)
             arr(hole) = item
         }
+        println(result)
     }
 
     def printArray(arr: Array[Int]) = println(arr.mkString(" "))
