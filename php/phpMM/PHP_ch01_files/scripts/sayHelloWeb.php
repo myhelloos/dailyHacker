cec26663fea3f9531d219fac94fdc7f86b8cc9cf
Alfred Yuan <yuanxun.cn@gmail.com>
2015-07-09 23:07:52 +0800

    update chapter 1

--- php/phpMM/PHP_ch01_files/scripts/sayHelloWeb.php
+++ php/phpMM/PHP_ch01_files/scripts/sayHelloWeb.php
@@ -1,17 +1,17 @@
-<html> 
- <head> 
-  <link href="../../css/phpMM.css" rel="stylesheet" type="text/css" /> 
- </head> 
- 
- <body> 
-  <div id="header"><h1>PHP & MySQL: The Missing Manual</h1></div> 
-  <div id="example">Example 1-1</div> 
- 
-  <div id="content"> 
-    <h1>Hello, <?php echo $_REQUEST['name']; ?></h1> 
-    <p>Great to meet you. Welcome to the beginning of your PHP programming odyssey.</p> 
-  </div> 
- 
-  <div id="footer"></div> 
- </body> 
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../../css/phpMM.css">
+	</head>
+	<body>
+		<div id="header"><h1>PHP & MySQL: The Missing Manual</h1></div>
+		<div id="example">Example 1-1</div>
+		
+		<div id="content">
+			<h1>Hello, <?php echo $_REQUEST['name']?></h1>
+			<P>Great to meet you. Welcome to the beginning of you PHP programming odyssey.</P>
+		</div>
+		
+		<div id="footer"></div>
+	</body>
 </html>
