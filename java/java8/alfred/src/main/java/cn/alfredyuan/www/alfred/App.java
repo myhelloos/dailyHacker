ba540b5fd84804d1864a86dea5a53c4b14caf837
Alfred Yuan <yuanxun.cn@gmail.com>
2015-08-15 11:45:00 +0800

    the basic framwork for java8 in paractice

---
+++ java/java8/alfred/src/main/java/cn/alfredyuan/www/alfred/App.java
@@ -0,0 +1,13 @@
+package cn.alfredyuan.www.alfred;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
